8b68a41b9f17dbe3613380772eaf08708aea0c34
Leandro Lima <leandro.lsilva@b2wdigital.com>
2021-02-12 13:29:37 -0300

    /mine method api

--- dev/api.js
+++ dev/api.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
+const uuid = require('uuid');
+const nodeAddress = uuid.v1().split('-').join('');
 const bitcoin = new Blockchain();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -9,13 +11,31 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.get('/blockchain', (req, res) => {
     res.send(bitcoin);
 });
+
 app.post('/transaction', (req, res) => {
     const blockIndex = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
     return res.json({'note' : `Transaction will be added in block ${blockIndex}`});
 });
-app.get('/mine', (req, res) => {
 
+app.get('/mine', (req, res) => {
+    const lastBlock = bitcoin.getLastBlock();
+    const previousBlockHash = lastBlock['hash'];
+    
+    const currentBlockData = {
+        transactions: bitcoin.pendingTransactions,
+        index: lastBlock['index'] + 1
+    };
+
+    const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
+    const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
+    bitcoin.createNewTransaction(12.5, "00", nodeAddress);
+    const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
+    res.json({
+        note: "New block mined successfully",
+        block: newBlock
+    });
 });
+
 app.listen(3000, () => {
     console.log('Listening port 3000');
 });
